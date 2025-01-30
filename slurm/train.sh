402af3aed91344ac6d651bbcc11994acfffce443
Kiameow <zjzootl@qq.com>
2025-01-30 15:00:14 +0800

    init ddpm02

---
+++ slurm/train.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -J zjz
+#SBATCH --output=output_%j.log         # Log file with job ID
+#SBATCH --error=error_%j.err           # Error log file with job ID
+#SBATCH --mail-type=ALL           # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=zhang_lumi@foxmail.com 
+#SBATCH -A F00120230017
+#SBATCH -p p-A800
+#SBATCH -N 1
+#SBATCH -c 1
+#SBATCH --gres=gpu:1
+
+# Load necessary modules
+module load cuda12.1/toolkit/12.1.1
+# source activate your_conda_env
+source ~/.bashrc
+conda activate zjz
+python diffusion_training.py RESUME_RECENT 01
